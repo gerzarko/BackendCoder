e7165d573182d11ad005f98c9ae5b40c06057561
ger <68071939+gerzarko@users.noreply.github.com>
2022-10-19 14:19:37 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,39 @@
+class Usuario{
+
+    constructor(nombre,apellido,mascotas= [],libros = []){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this. mascotas = mascotas;
+        this.libros = libros;
+
+    }
+
+    getFullName(){
+        return `${this.nombre} ${this.apellido}`;
+    }
+
+    addMascota(mascota){
+        this.mascotas.push(mascota);
+    }
+
+    countMascotas(){
+        return `${this.mascotas.length}`
+    }
+
+    addBook(libro){
+        this.libros.push(libro)
+    }
+
+    getBookNames(){
+        return this.libros.map(libro =>`${libro.nombre}`);
+    }
+
+
+}
+
+
+const usuario1 = new Usuario("German","Zarkovich");
+usuario1.addBook({libro: "A la deriva", autor: "horacio Quiroga"});
+
+console.log(JSON.stringify(usuario1));
+console.log(usuario1.libros[0].libro);
